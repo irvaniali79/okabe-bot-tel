2aaadcd1906a8e582d10e151e7cfacb82d4a8c09
okabe <irvaniali79@gmail.com>
2021-12-01 17:49:01 +0330

    age calculator && qrcode function

--- okabe-rintaro-bot.py
+++ okabe-rintaro-bot.py
@@ -1,5 +1,7 @@
 import telebot
 import random
+import khayyam
+import qrcode
 
 bot = telebot.TeleBot('2140486942:AAFJgBA0y10ZHkbM-7VIIfUm6B3-WMBnsAk')
 number=0
@@ -8,13 +10,7 @@ number=0
 @bot.message_handler(commands=['start'])
 def register(message):
     bot.reply_to(message, "Welcome to my bot " +  message.from_user.first_name +  " !")
-
-@bot.message_handler(commands=['game'])
-def game(message):
-    num=random.randint(0,100)
-    markup = telebot.types.ForceReply(selective=False)
-    bot.send_message(message.chat.id, "guess my number:", reply_markup=markup)
-    
+   
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.reply_to(message,"""
@@ -53,8 +49,6 @@ def geuess(message):
     elif int(message.text) == number:
         bot.reply_to(message, "you guess right")
 
-
-
 @bot.message_handler(commands=['max'])
 def getarray(message):
     arr=bot.send_message(message.chat.id,'Enter your array for example :1 2 3 4')
@@ -73,6 +67,26 @@ def max_index_array(message):
     numbers=list(map(int,message.text.split()))
     bot.send_message(message.chat.id,numbers.index(max(numbers)))
 
+@bot.message_handler(commands=['age'])
+def age(message):
+    bot.reply_to(message,"Please enter your birth date ,use seprator \'/\' like 1379/12/22 ") 
+    bot.register_next_step_handler(message ,agecalculator)
 
+def agecalculator(message):
+    date = message.text.split('/')
+    dif = JalaliDatetime.now()-JalaliDatetime(date[0],date[1],date[2])
+    bot.reply_to(message,dif)
+
+
+@bot.message_handler(commands=['qrcode'])
+def text2qrcode(message):
+  bot.reply_to(message,"Enter your text :") 
+  bot.register_next_step_handler(message ,toqrcode)
+
+def toqrcode(message):
+    img = qrcode.make(message.text) 
+    img.save('qrcode.png')
+    photo = open('qrcode.png', 'rb')
+    bot.send_photo(message.chat.id, photo)
 
 bot.infinity_polling()
