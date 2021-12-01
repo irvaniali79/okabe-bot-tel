87c36cef1c41b779a5f37f68b9719653d02c014e
okabe <irvaniali79@gmail.com>
2021-12-01 17:13:49 +0330

    max and maxindex function

--- okabe-rintaro-bot.py
+++ okabe-rintaro-bot.py
@@ -28,11 +28,32 @@ def help(message):
     maximum of array \n
     /max\n
     position of maximum number of array\n
-    /argmax\n
+    /maxindex\n
     Text2QRcode\n
     /qrcode\n
     Guide\n
     /help\n
     """)
+
+
+@bot.message_handler(commands=['max'])
+def getarray(message):
+    arr=bot.send_message(message.chat.id,'Enter your array for example :1 2 3 4')
+    bot.register_next_step_handler(arr,max_array)
+    
+def max_array(message):    
+    numbers=list(map(int,message.text.split()))
+    bot.send_message(message.chat.id,max(numbers))
+
+@bot.message_handler(commands=['maxindex'])
+def getarray(message):
+    arr=bot.send_message(message.chat.id,'Enter your array for example :1 2 3 4')
+    bot.register_next_step_handler(arr,max_index_array)
+    
+def max_index_array(message):    
+    numbers=list(map(int,message.text.split()))
+    bot.send_message(message.chat.id,numbers.index(max(numbers)))
+
+
 
 bot.infinity_polling()
