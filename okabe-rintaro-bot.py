9e7e29c30995e84cb7ce689cb4c01ddb0a81a9bd
okabe <irvaniali79@gmail.com>
2021-12-01 16:39:34 +0330

    adding help function

--- okabe-rintaro-bot.py
+++ okabe-rintaro-bot.py
@@ -14,5 +14,25 @@ def game(message):
     markup = telebot.types.ForceReply(selective=False)
     bot.send_message(message.chat.id, "guess my number:", reply_markup=markup)
     
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message,"""
+    Wellcome\n
+    /start\n
+    Guess game\n
+    /game \n
+    how old are you? \n
+    /age\n
+    Sentence2voice\n
+    /voice\n
+    maximum of array \n
+    /max\n
+    position of maximum number of array\n
+    /argmax\n
+    Text2QRcode\n
+    /qrcode\n
+    Guide\n
+    /help\n
+    """)
 
 bot.infinity_polling()
